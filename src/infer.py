2a7a4751c1c9fc2052d654cb8658a753cc1bcd98
st81 <m59rt@yahoo.co.jp>
2022-09-20 13:02:19 +0900

    Fix: Make config.base.output dir when save submission

--- src/infer.py
+++ src/infer.py
@@ -57,4 +57,5 @@ if __name__ == "__main__":
 
     submission = create_submission(df["discourse_id"].values, preds)
     print(submission)
+    Path(config.base.output_dir)
     submission.to_csv(Path(config.base.output_dir) / FILENAME.SUBMISSION, index=False)
